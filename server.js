46fa4b77f080a2ebb57a95aeeb4344354ab11855
Surabhi-G <60303068+Surabhi-G@users.noreply.github.com>
2020-05-22 20:32:11 -0400

    condition if zip is null
    Minor changes for zip and event added on button in HTML

--- server.js
+++ server.js
@@ -1,36 +1,44 @@
 // Setup empty JS object to act as endpoint for all routes
+
 const projectData = [];
 
 // Require Express to run server and routes
+
 const express= require('express');
+
 // Start up an instance of app
+
 const app = express();
 const bodyParser= require('body-parser');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+
 const cors=require('cors');
 app.use(cors());
-// Initialize the main project folder
-app.use(express.static('website'));
 
+// Initialize the main project folder
+
+app.use(express.static('website'));
 const port = 8080;
 const server= app.listen(port, listening);
+
 // Setup Server
+
 function listening(){
     console.log(`server is up and running on ${port}`);
 }
+
 app.get('/all',function(req, res){
     res.send(projectData);
 })
 
 app.post('/addData', function (req, res){
-    newEntry = {
-         date: req.body.date,
-         temp: req.body.temp,
-         userInput: req.body.userInput
-        };
+    newEntry = {date: req.body.date, temp: req.body.temp, userInput: req.body.userInput};
     projectData.unshift(newEntry);    
 })
--- website/app.js
+++ website/app.js
@@ -1,6 +1,7 @@
 /* Global Variables */
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-let apiKey = 'f93508f5516670154f8d3574002efac4';
+
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'f93508f5516670154f8d3574002efac4';
 
 // Create a new date instance dynamically with JS
 
@@ -13,16 +14,21 @@ function performAction(event){
     let zip = document.getElementById('zip').value;
     let userInput = document.getElementById('feelings').value;
 
-    getData(baseURL, zip, apiKey)
-    .then(function(weatherData){
-        console.log(weatherData);
-        const temperature = weatherData.main.temp;
-        postData('/addData', 
+    if(isNaN(parseFloat(zip))){
+        alert("Please enter zip");
+    }
+    else{
+        getData(baseURL, zip, apiKey)
+        .then(function(weatherData){
+            console.log(weatherData);
+            const temperature = weatherData.main.temp;
+            postData('/addData', 
                 {date: newDate, 
                 temp: temperature, 
                 userInput: userInput});       
-        updateUI('/all');
-    }) 
+            updateUI('/all');
+        }) 
+    }   
 };
 
 const getData= async(baseURL, zip, apiKey)=>{
@@ -50,10 +56,12 @@ const postData = async(url = '', data = {})=>{
         const newData= await res.json();
         console.log(newData)
         return newData;
-    }catch(error){
+    }
+    catch(error){
         console.log("error",error);
     }
 };
+
 const updateUI = async (url='') => {
     const req = await fetch(url);
     try{
@@ -62,8 +70,8 @@ const updateUI = async (url='') => {
       document.getElementById('date').innerHTML = 'Today is : '+ allData[0].date;
       document.getElementById('temp').innerHTML = 'Current temparature is : '+ allData[0].temp;
       document.getElementById('content').innerHTML = 'I am feeling : ' + allData[0].userInput;
-  
-    }catch(error){
+    }
+    catch(error){
       console.log("error", error);
     }
 };
